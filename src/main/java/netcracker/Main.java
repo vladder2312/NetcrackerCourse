efa145e05b52810bc37b5e032b040cde6a836d3d
Владислав Стяжкин <vladder2312@mail.ru>
2020-11-18 12:11:08 +0300

    Перенос методов заполнения репозитория из Main в ContractRepository

--- src/main/java/netcracker/Main.java
+++ src/main/java/netcracker/Main.java
@@ -2,9 +2,7 @@ package netcracker;
 
 import netcracker.contract.*;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author Vladislav_Styazhkin
@@ -12,10 +10,8 @@ import java.util.UUID;
  */
 public class Main {
 
-    /**
-     * Repository that contains contracts
-     */
     private static final ContractRepository repository = new ContractRepository();
+    private static final Scanner in = new Scanner(System.in);
 
     /**
      * Start point of the program
@@ -23,69 +19,28 @@ public class Main {
      * @param args command line values
      */
     public static void main(String[] args) {
-        fillRepository(50);
+        System.out.println(ChannelPackage.KIDS.toString());
+        repository.fillRepository(50);
         for (Contract c : repository.getContracts()) {
             System.out.println(c);
         }
+        while (true){
+            System.out.println("1. Поиск\n2. Сортировка\n3. Выход\n>");
+            int selected = in.nextInt();
+            switch (selected){
+                case 1 -> searchDialog();
+                case 2 -> sortDialog();
+                case 3 -> { return; }
+                default -> System.out.println("Ошибка ввода");
+            }
+        }
     }
 
-    /**
-     * Creates new randomly generated contract
-     *
-     * @return new contract
-     */
-    private static Contract createRandomContract() {
-        Contract contract = null;
-        int id = UUID.randomUUID().hashCode();
-        Calendar start = new GregorianCalendar(2000 + (int) (Math.random() * 20), (int) (Math.random() * 12), (int) (Math.random() * 29));
-        Calendar end = (Calendar) start.clone();
-        end.add(Calendar.YEAR, (int) (Math.random() * 10));
-        end.add(Calendar.MONTH, (int) (Math.random() * 12));
-        end.add(Calendar.DAY_OF_MONTH, (int) (Math.random() * 31));
-        Client client = new Client(
-                UUID.randomUUID().hashCode(),
-                new GregorianCalendar(1950 + (int) (Math.random() * 50), (int) (Math.random() * 12), (int) (Math.random() * 29)),
-                Data.fioList.get((int) (Math.random() * Data.fioList.size())),
-                Math.random() * 2 > 1 ? "Male" : "Female",
-                (1000 + (int) (Math.random() * 9000)) + " " + (100000 + (int) (Math.random() * 900000))
-        );
+    private static void searchDialog() {
 
-        switch ((int) (Math.random() * 3)) {
-            case 0 -> contract = new MobileContract(
-                    id,
-                    start,
-                    end,
-                    client,
-                    100 + (int) (Math.random() * 1000),
-                    1024 + (int) (Math.random() * 10000),
-                    100 + (int) (Math.random() * 1000)
-            );
-            case 1 -> contract = new TeleContract(
-                    id,
-                    start,
-                    end,
-                    client,
-                    ChannelPackage.values()[(int) (Math.random() * ChannelPackage.values().length)]
-            );
-            case 2 -> contract = new InternetContract(
-                    id,
-                    start,
-                    end,
-                    client,
-                    (int) (20 + Math.random() * 300)
-            );
-        }
-        return contract;
     }
 
-    /**
-     * Fills the contracts repository
-     *
-     * @param amount amount of contracts
-     */
-    private static void fillRepository(int amount) {
-        for (int i = 0; i < amount; i++) {
-            repository.addContract(createRandomContract());
-        }
+    private static void sortDialog() {
+
     }
 }
--- src/main/java/netcracker/contract/ContractRepository.java
+++ src/main/java/netcracker/contract/ContractRepository.java
@@ -1,6 +1,12 @@
 package netcracker.contract;
 
+import netcracker.Client;
+import netcracker.Data;
 import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.UUID;
 
 /**
  * Repository for storing contracts
@@ -31,10 +37,21 @@ public class ContractRepository {
     }
 
     /**
-     * Get a netcracker.contract by its ID
+     * Fills the contracts repository
      *
-     * @param id netcracker.contract ID
-     * @return netcracker.contract with a specified ID
+     * @param amount amount of contracts
+     */
+    public void fillRepository(int amount) {
+        for (int i = 0; i < amount; i++) {
+            addContract(createRandomContract());
+        }
+    }
+
+    /**
+     * Get a contract by its ID
+     *
+     * @param id contract ID
+     * @return contract with a specified ID
      */
     public Contract getContractByID(long id) {
         for (Contract c : contracts) {
@@ -46,9 +63,9 @@ public class ContractRepository {
     }
 
     /**
-     * Add a netcracker.contract to repository
+     * Add a contract to repository
      *
-     * @param contract new netcracker.contract
+     * @param contract new contract
      */
     public void addContract(Contract contract) {
         if (amount == contracts.length) {
@@ -63,9 +80,9 @@ public class ContractRepository {
     }
 
     /**
-     * Remove a netcracker.contract from repository
+     * Remove a contract from repository
      *
-     * @param id id of netcracker.contract that need to be removed
+     * @param id id of contract that need to be removed
      */
     public void deleteContract(long id) {
         for (Contract c : contracts) {
@@ -74,5 +91,54 @@ public class ContractRepository {
                 return;
             }
         }
+    }
+
+    /**
+     * Creates new randomly generated contract
+     *
+     * @return new contract
+     */
+    private static Contract createRandomContract() {
+        Contract contract = null;
+        int id = UUID.randomUUID().hashCode();
+        Calendar start = new GregorianCalendar(2000 + (int) (Math.random() * 20), (int) (Math.random() * 12), (int) (Math.random() * 29));
+        Calendar end = (Calendar) start.clone();
+        end.add(Calendar.YEAR, (int) (Math.random() * 10));
+        end.add(Calendar.MONTH, (int) (Math.random() * 12));
+        end.add(Calendar.DAY_OF_MONTH, (int) (Math.random() * 31));
+        Client client = new Client(
+                UUID.randomUUID().hashCode(),
+                new GregorianCalendar(1950 + (int) (Math.random() * 50), (int) (Math.random() * 12), (int) (Math.random() * 29)),
+                Data.fioList.get((int) (Math.random() * Data.fioList.size())),
+                Math.random() * 2 > 1 ? "Male" : "Female",
+                (1000 + (int) (Math.random() * 9000)) + " " + (100000 + (int) (Math.random() * 900000))
+        );
+
+        switch ((int) (Math.random() * 3)) {
+            case 0 -> contract = new MobileContract(
+                    id,
+                    start,
+                    end,
+                    client,
+                    100 + (int) (Math.random() * 1000),
+                    1024 + (int) (Math.random() * 10000),
+                    100 + (int) (Math.random() * 1000)
+            );
+            case 1 -> contract = new TeleContract(
+                    id,
+                    start,
+                    end,
+                    client,
+                    ChannelPackage.values()[(int) (Math.random() * ChannelPackage.values().length)]
+            );
+            case 2 -> contract = new InternetContract(
+                    id,
+                    start,
+                    end,
+                    client,
+                    (int) (20 + Math.random() * 300)
+            );
+        }
+        return contract;
     }
 }
